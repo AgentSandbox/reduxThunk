99cbb625e5438003fe2297feca59adef432b213a
Danil <bion-1@mail.ru>
2024-01-07 17:17:39 +0100

    com

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {useState} from "react";
 import './App.css'
 import {v1} from "uuid"
 import {useDispatch, useSelector} from "react-redux"
-import {addTodo} from "./store/todoSlice.jsx";
+import {addTodo, deleteTodo, toggleTodo} from "./store/todoSlice.jsx";
 
 
 function App() {
@@ -19,19 +19,37 @@ function App() {
                 completed: false
             }))
         }
+        setText("")
     }
     return (
         <div className="App">
             <label>
-                <input value={text} onChange={(e) => setText(e.target.value)} type="text"/>
+                <input
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    type="text"
+                />
                 <button onClick={onClickHandle}>ADD TODO</button>
             </label>
             <ul>
                 {
                     textTodo.map(t => <li key={t.id}>
-                        <input type="checkbox"/>
-                        <span>{t.text}</span>
-                        <span style={{color: 'red'}}>&times;</span>
+                        <input
+                            onClick={()=>{dispatch(toggleTodo(t.id))}}
+                            type="checkbox"
+                            checked={t.completed}
+                        />
+                        <span
+                            className={t.completed?"textCompleted":""}
+                        >
+                            {t.text}
+                        </span>
+                        <span
+                            onClick={()=>{dispatch(deleteTodo(t.id))}}
+                            className="delete"
+                        >
+                            &times;
+                        </span>
                     </li>)
                 }
             </ul>
--- src/store/todoSlice.jsx
+++ src/store/todoSlice.jsx
@@ -5,14 +5,21 @@ const initialState = {
 }
 
 export const todoSlice = createSlice({
-    name: "todos",
+    name: "to",
     initialState,
     reducers: {
         addTodo: (state, action) => {
             state.arr.push(action.payload)
+        },
+        toggleTodo: (state, action) => {
+            const compl = state.arr.find((t)=>t.id === action.payload)
+            compl.completed = !compl.completed
+        },
+        deleteTodo: (state, action) => {
+            state.arr = state.arr.filter((t) => t.id !== action.payload)
         }
     }
 })
 
-export const{addTodo} = todoSlice.actions
+export const{addTodo, toggleTodo, deleteTodo} = todoSlice.actions
 export default todoSlice.reducer
